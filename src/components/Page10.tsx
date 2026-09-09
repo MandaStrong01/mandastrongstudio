4091c7a9c17c30bb39fb23668016fdddf895eb18
MandaStrong01 <woolleya129@gmail.com>
2026-09-09 14:01:28 -0400

    Fix typo in domain reference

--- src/components/Page10.tsx
+++ src/components/Page10.tsx
@@ -7,7 +7,7 @@ interface Page10Props {
 }
 
 export default function Page10({ onNext, onBack }: Page10Props) {
-  const [videoUrl] = useState<string>('https://mandsstrongmovies.bolt.host/');
+  const [videoUrl] = useState<string>('https://mandastrongmovies101.bolt.host/');
 
   const handleOpenFullscreen = () => {
     window.open(videoUrl, '_blank');
